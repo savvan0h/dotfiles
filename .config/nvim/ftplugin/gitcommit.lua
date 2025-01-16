56774dda1764e886bee275599998865a16eca43e
savvan0h <hiroki.sawano.2512@gmail.com>
2025-01-17 02:05:35 +0900

    Use git:staged to include staged changes in chat context

--- .config/nvim/ftplugin/gitcommit.lua
+++ .config/nvim/ftplugin/gitcommit.lua
@@ -4,10 +4,7 @@ if has_copilot then
   vim.schedule(function()
       local chat = require("CopilotChat")
       chat.open()
-      chat.ask('Generate a concise and meaningful commit message title based on the changes made. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.', {
-        selection = function(source)
-          return require("CopilotChat.select").gitdiff(source, true)
-        end,
+      chat.ask('> #git:staged\n\nGenerate a concise and meaningful commit message title based on the changes made. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.', {
         callback = function(response)
           chat.close()
           vim.api.nvim_buf_set_text(0, 0, 0, 0, 0, {response})
